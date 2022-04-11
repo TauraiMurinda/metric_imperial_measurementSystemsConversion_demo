d7ae3907d708ed3be6ced8edc5a99d1aa89406ee
taurai murinda <murindataurai@gmail.com>
2022-04-11 18:43:37 +0200

    123 test

--- src/main/java/com/ayo4u/assessment/metric_imperial_measurementSystemsConversion_demo/MetricImperialMeasurementSystemsConversionDemoApplication.java
+++ src/main/java/com/ayo4u/assessment/metric_imperial_measurementSystemsConversion_demo/MetricImperialMeasurementSystemsConversionDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MetricImperialMeasurementSystemsConversionDemoApplication {
 
 	public static void main(String[] args) {
-	 SpringApplication.run( MetricImperialMeasurementSystemsConversionDemoApplication.class, args);
+	 SpringApplication.run(MetricImperialMeasurementSystemsConversionDemoApplication.class, args);
 	}
 
 }
