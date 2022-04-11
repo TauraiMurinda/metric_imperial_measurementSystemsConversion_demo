90cd8ea19bf8119c1dc65380ac8db08654d56a67
k <k@k-ZenBook-UX363EA-UX363EA>
2022-04-11 17:29:17 +0200

    project init

---
+++ src/main/java/com/ayo4u/assessment/metric_imperial_measurementSystemsConversion_demo/MetricImperialMeasurementSystemsConversionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ayo4u.assessment.metric_imperial_measurementSystemsConversion_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricImperialMeasurementSystemsConversionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricImperialMeasurementSystemsConversionDemoApplication.class, args);
+	}
+
+}
