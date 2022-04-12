e58f3091504a5d6240d47fe2394697c83b3663f2
taurai murinda <murindataurai@gmail.com>
2022-04-12 07:20:22 +0200

    functionalities

---
+++ src/main/java/com/ayo4u/assessment/service/convertor/utility/ConversionType.java
@@ -0,0 +1,9 @@
+package com.ayo4u.assessment.service.convertor.utility;
+
+public enum ConversionType {
+    TEMPERATURE,
+    AREA,
+    LENGTH,
+    VOLUME,
+    WEIGHT
+}
---
+++ src/main/java/com/ayo4u/assessment/service/convertor/utility/SystemType.java
@@ -0,0 +1,6 @@
+package com.ayo4u.assessment.service.convertor.utility;
+
+public enum SystemType {
+    IMPERIAL,
+    METRIC
+}
